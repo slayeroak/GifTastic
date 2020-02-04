bcb746650aa4870496dd75d6e80a6b549aab5464
slayeroak <roakley1@villanova.edu>
2020-02-04 14:32:10 -0500

    need to figure out why .trim is undefined on line 18

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,82 @@
-$(document).ready(function() {
-    var gifSearches = [];
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + car + "zJKwX8G4J6UjTjSQpTBVFFDt8Wj0nX6U";
-    // console.log(queryURL);
-        $.ajax({url: queryURL, method: 'GET'})
-        .done(function(response) {
-            // grabs the data
-            var results = response.data;
-            console.log(results);
-        }
+$(document).ready(function () {
+	var movies = ["moulin rouge", "clueless", "mean girls", "drop dead fred", "breakfast at tiffanys", "cruel intentions", "donnie darko", "a night at the roxbury"];
+
+	// Add buttons for original movies array
+	function renderButtons() {
+		$("#div-buttons").empty();
+		for (i = 0; i < movies.length; i++) {
+			$("#div-buttons").append("<button class='btn btn-success' data-movie='" + movies[i] + "'>" + movies[i] + "</button>");
+		}
+	}
+
+	renderButtons();
+
+	// Adding a button for movie entered
+	$("#addGIF").on("click", function () {
+		event.preventDefault();
+		var movie = $("#movieInput").val().trim()
+		movies.push(movie);
+		renderButtons();
+		return;
+	});
+
+
+	// Getting gifs from api... onto html
+	$("button").on("click", function () {
+		var movie = $(this).attr("data-movie");
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+			movie + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function (response) {
+			var results = response.data;
+			$("#gifView").empty();
+			for (var i = 0; i < results.length; i++) {
+				var movieDiv = $("<div>");
+				var p = $("<p>").text("Rating: " + results[i].rating);
+				var movieImg = $("<img>");
+
+				movieImg.attr("src", results[i].images.original_still.url);
+				movieImg.attr("data-still", results[i].images.original_still.url);
+				movieImg.attr("data-animate", results[i].images.original.url);
+				movieImg.attr("data-state", "still");
+				movieImg.attr("class", "gif");
+				movieDiv.append(p);
+				movieDiv.append(movieImg);
+				$("#gifView").append(movieDiv);
+			}
+		});
+	});
+
+	function changeState(){
+		var state = $(this).attr("data-state");
+		var animateImage = $(this).attr("data-animate");
+		var stillImage = $(this).attr("data-still");
+
+		if (state == "still") {
+			$(this).attr("src", animateImage);
+			$(this).attr("data-state", "animate");
+		}
+
+		else if (state == "animate") {
+			$(this).attr("src", stillImage);
+			$(this).attr("data-state", "still");
+		}
+	}
+
+	// $("img").on("click", function() {
+	// 	console.log("click worked!");
+	// 	var src = movieImg.attr(src);
+	// 	src = src.substring(0, src.length - 10);
+	// 	src += ".url";
+	// 	console.log(src);
+	// 	movieImg.attr("src", src);
+	// });
+
+	// $(document).on("click", "#input", displayImg);
+	$(document).on("click", ".gif", changeState);
+
 });
